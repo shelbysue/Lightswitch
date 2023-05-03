e34da2d750a174ba869cad10e5cdb53e95c87d19
Shelby Wright-Howard <shelbysue1994@gmail.com>
2023-05-03 18:39:52 -0400

    src/adDesigner.css 	src/adDesigner.tsx 	src/header.css 	src/header.tsx 	src/votes.tsx

---
+++ src/Switch.tsx
@@ -0,0 +1,13 @@
+import './Switch.css';
+
+function Switch(){
+    return (
+        <div className="Switch">
+        <h1>switch</h1>
+        <button>on</button>
+        <button>off</button>
+        </div>
+    );
+}
+
+export default Switch; 
